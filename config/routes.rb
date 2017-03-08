e272e2e59bad3d8037f52f203529f596b790e221
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-03-08 20:10:24 +0700

    milestone 3

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
     resources :messages
   end
   root 'rooms#index'
+  get 'logout' =>'home#logout'
+  get 'login' => 'home#login'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
